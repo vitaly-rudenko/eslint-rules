1d7ab051302884a3f2c0584225846d3708e5ad3a
Vitaly Rudenko <maurice.dederick@gmail.com>
2024-10-27 18:07:30 +0200

    Improve infer attributes requirement

--- eslint-plugins/sequelize/require-infer-attributes-in-models.js
+++ eslint-plugins/sequelize/require-infer-attributes-in-models.js
@@ -1,6 +1,8 @@
 /**
- * Best way to define Sequelize models is using `extends Model<InferAttributes<M>, InferCreationAttribute<M>>`.
+ * The best way to define Sequelize models is with `Model<InferAttributes<M>, InferCreationAttribute<M>>`.
  * Reason: consistency and strict type checking, as well as allowing usage of `NonAttribute` and `CreationOptional` types.
+ *
+ * This rule is simple: require `InferAttributes` and `InferCreationAttributes` to be imported when `Model` and `Table` are imported.
  *
  * @type {import('eslint').Rule.RuleModule}
  */
@@ -15,33 +17,40 @@ export const requireInferAttributesInModelsRule = {
   create(context) {
     let hasInferAttributes = false;
     let hasInferCreationAttributes = false;
+    let hasModelImport = false;
+    let hasTableImport = false;
 
     return {
       Program() {
         hasInferAttributes = false;
         hasInferCreationAttributes = false;
+        hasModelImport = false;
+        hasTableImport = false;
       },
       ImportDeclaration(node) {
         if (node.source.value === 'sequelize') {
-          node.specifiers.forEach(specifier => {
-            if (specifier.type === 'ImportSpecifier') {
-              if (specifier.imported.name === 'InferAttributes') {
-                hasInferAttributes = true;
-              }
-              if (specifier.imported.name === 'InferCreationAttributes') {
-                hasInferCreationAttributes = true;
-              }
-            }
-          });
+          for (const specifier of node.specifiers) {
+            if (specifier.type !== 'ImportSpecifier') continue;
+
+            hasInferAttributes ||= specifier.imported.name === 'InferAttributes';
+            hasInferCreationAttributes ||= specifier.imported.name === 'InferCreationAttributes';
+          }
+        }
+
+        if (node.source.value === 'sequelize-typescript') {
+          for (const specifier of node.specifiers) {
+            if (specifier.type !== 'ImportSpecifier') continue;
+
+            hasModelImport ||= specifier.imported.name === 'Model';
+            hasTableImport ||= specifier.imported.name === 'Table';
+          }
         }
       },
       'Program:exit'(node) {
-        if (!hasInferAttributes || !hasInferCreationAttributes) {
-          context.report({
-            node,
-            messageId: 'missingInferAttributesInModel'
-          });
-        }
+        if (!hasModelImport || !hasTableImport) return;
+        if (hasInferAttributes && hasInferCreationAttributes) return;
+
+        context.report({ node, messageId: 'missingInferAttributesInModel' });
       }
     };
   }
--- eslint-plugins/sequelize/require-infer-attributes-in-models.test.js
+++ eslint-plugins/sequelize/require-infer-attributes-in-models.test.js
@@ -3,50 +3,52 @@ import { requireInferAttributesInModelsRule } from './require-infer-attributes-i
 
 ruleTester.run('require-infer-attributes-in-models', requireInferAttributesInModelsRule, {
   valid: [
+    // No Model or Table import - rule shouldn't apply
+    {
+      code: `
+        import { DataTypes } from 'sequelize';
+      `,
+    },
+    // Only Model without Table - rule shouldn't apply
+    {
+      code: `
+        import { Model } from 'sequelize-typescript';
+        import { DataTypes, InferAttributes, InferCreationAttributes } from 'sequelize';
+      `,
+    },
+    // Has both required imports
     {
       code: `
         import { DataTypes, InferAttributes, InferCreationAttributes } from 'sequelize';
-        import { Model } from 'sequelize-typescript';
+        import { Model, Table } from 'sequelize-typescript';
       `,
     },
+    // Has both required imports with type syntax
     {
       code: `
-        import {
-          DataTypes,
-          type InferAttributes,
-          type InferCreationAttributes,
-          Model
-        } from 'sequelize';
+        import { DataTypes, type InferAttributes, type InferCreationAttributes } from 'sequelize';
+        import { Model, Table } from 'sequelize-typescript';
       `,
     },
   ],
   invalid: [
-    {
-      code: `
-        import { DataTypes } from 'sequelize';
-        import { Model } from 'sequelize-typescript';
-      `,
-      errors: [
-        {
-          messageId: 'missingInferAttributesInModel',
-        },
-      ],
-    },
-    {
-      code: `
-        import { DataTypes, InferAttributes } from 'sequelize';
-        import { Model } from 'sequelize-typescript';
-      `,
-      errors: [
-        {
-          messageId: 'missingInferAttributesInModel',
-        },
-      ],
-    },
+    // Missing both InferAttributes
     {
       code: `
         import { DataTypes, InferCreationAttributes } from 'sequelize';
-        import { Model } from 'sequelize-typescript';
+        import { Model, Table } from 'sequelize-typescript';
+      `,
+      errors: [
+        {
+          messageId: 'missingInferAttributesInModel',
+        },
+      ],
+    },
+    // Missing InferCreationAttributes
+    {
+      code: `
+        import { DataTypes, InferAttributes } from 'sequelize';
+        import { Model, Table } from 'sequelize-typescript';
       `,
       errors: [
         {
